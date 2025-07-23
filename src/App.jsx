cc62ad50e0a2145d6bdf063cf5a5cec15f4f4966
MeGallin <me@garyallin.uk>
2025-07-23 12:59:06 +0100

    Inistal commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World - AI TFL</h1>
+      <p>Welcome to the AI TFL subdomain application!</p>
+    </div>
+  )
+}
+
+export default App
